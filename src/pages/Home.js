b39f2bc7bda102bd7a88e4cae7605f4cb29e2da5
Kaká Cordovil <kakacordovil@gmail.com>
2021-05-12 01:21:49 +0100

    Routes

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./css/Home.css";
+
+function Home () {
+    return (
+        <div className="home">
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default Home;
